1239de00cbb8fb3651f7fa17c343f492cc3dbcd0
TOTO05-a <ac138439@gmail.com>
2026-03-05 23:40:56 -0600

    feat: se agregaron ejemplos de js, el mas importante como cambiar estilos desde js

---
+++ 01-DOM-basics/script.js
@@ -0,0 +1,9 @@
+const firstItem = DocumentTimeline.querySelector(".item")
+
+
+console.log(firstItem)
+console.log(firstItem.parentElement)
+console.log(firstItem.parentElement.parentElement)
+
+console.log(firstItem.closest("sections"))
+
---
+++ 01-DOM-basics/scriptEduin.js
@@ -0,0 +1,21 @@
+const elemento = document.getElementById("titulo");
+elemento.innerHTML = 'Hola!'
+
+elemento.style.color = "red"; "border: 1px solid red"
+// elemento.style.color = "red"
+
+const articulos = document.getElementsByClassName('titulo')
+
+console.log(titulo)
+
+
+// for ( let i = 0; i < articulos.length; i++){
+//     articulos[i].innerHTML = Articulo ${i+1};
+// }
+
+const boton = document.getElementById("boton")
+const texto = document.getElementById("texto")
+
+boton.addEventListener("click", function(){
+    console.log(textContent = "HICISTE CLICK")
+})
