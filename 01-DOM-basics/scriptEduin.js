780366d55220fd92558b406a69e3344183daf448
TOTO05-a <ac138439@gmail.com>
2026-03-06 00:52:21 -0600

    refactor: se agrego un ejemplo de la empresa acme donde se usa una tabla y se le agregan funcionalidades para insertar datos

--- 01-DOM-basics/scriptEduin.js
+++ 01-DOM-basics/scriptEduin.js
@@ -1,21 +1,45 @@
-const elemento = document.getElementById("titulo");
-elemento.innerHTML = 'Hola!'
-
-elemento.style.color = "red"; "border: 1px solid red"
-// elemento.style.color = "red"
-
-const articulos = document.getElementsByClassName('titulo')
-
-console.log(titulo)
-
-
-// for ( let i = 0; i < articulos.length; i++){
-//     articulos[i].innerHTML = Articulo ${i+1};
-// }
-
-const boton = document.getElementById("boton")
-const texto = document.getElementById("texto")
-
+const boton = document.getElementById("btnAgregar")
+const tabla = document.getElementById("tablaProductos")
 boton.addEventListener("click", function(){
-    console.log(textContent = "HICISTE CLICK")
-})
+    alert("se hizo click en agregar")
+
+
+let codigo = prompt("Ingrese el codigo")
+let nombre = prompt("Ingrese el nombre")
+let precio = prompt("Ingrese el precio")
+
+tabla.innerHTML += `
+<tr>
+    <td>${codigo}</td>
+    <td>${nombre}</td>
+    <td>${precio}</td>
+    <td>
+        <button>Editar</button>
+        <button>borrar</button>
+
+    </td>
+</tr>
+    `
+}) 
+// const elemento = document.getElementById("titulo");
+// elemento.innerHTML = 'Hola!'
+
+// elemento.style.color = "red"; "border: 1px solid red"
+// // elemento.style.color = "red"
+
+// const articulos = document.getElementsByClassName('titulo')
+
+// console.log(titulo)
+
+
+// // for ( let i = 0; i < articulos.length; i++){
+// //     articulos[i].innerHTML = Articulo ${i+1};
+// // }
+
+// const boton = document.getElementById("boton")
+// const texto = document.getElementById("texto")
+
+// boton.addEventListener("click", function(){
+//     console.log(textContent = "HICISTE CLICK")
+// })
+
