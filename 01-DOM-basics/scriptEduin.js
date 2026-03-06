1ee79eab4461152d039a7f276e7a672888a5a990
TOTO05-a <ac138439@gmail.com>
2026-03-06 02:07:27 -0600

    feat: se agregaron opciones y un form

--- 01-DOM-basics/scriptEduin.js
+++ 01-DOM-basics/scriptEduin.js
@@ -1,5 +1,31 @@
-const boton = document.getElementById("btnAgregar")
+const productos =[]
+const form = document.getElementById("formProducto")
 const tabla = document.getElementById("tablaProductos")
+
+// porque usamos submit Cuando presionas el botón: Se dispara el evento:
+form.addEventListener("submit",function(e){
+//     ¿Por qué usamos preventDefault()? Porque los formularios normalmente recargan la página
+// Eso borraría tus datos.
+// Entonces usamos:
+    e.preventDefault()
+
+    let codigo = document.getElementById("codigo").value
+    let nombre = document.getElementById("nombre").value
+    let precio = document.getElementById("precio").value
+
+    const producto = {
+        codigo: codigo,
+        nombre: nombre,
+        precio: precio,
+    }
+    productos.push(producto)
+    renderizarTabla()
+})
+
+
+
+const boton = document.getElementById("btnAgregar")
+
 boton.addEventListener("click", function(){
     alert("se hizo click en agregar")
 
@@ -27,6 +53,26 @@ document.addEventListener("click", function(e){
         e.target.parentElement.parentElement.remove()
     }
 })
+
+// se debe agregar una funcion de renderice la tabla para poder hacer el del boton de editar
+
+function renderizarTabla(){
+    tabla.innerHTML += ""
+    productos.forEach(function(producto, index){
+        tabla.innerHTML += `
+        <tr>
+        <td>${producto.codigo}</td>
+        <td>${producto.nombre}</td>
+        <td>${producto.precio}</td>
+        <td>
+        <button class = "editar" data-index="${index}"Editar</button>
+        <button class = "borrar" data-index="${index}"Borrar</button>
+        </td>
+        </tr>
+        `
+    })
+}
+
 // const elemento = document.getElementById("titulo");
 // elemento.innerHTML = 'Hola!'
 
